19f0cda43ee543ddcf35bafc1374f9d7505f4c6b
Leonardo Idone <idone.leonardo@gmail.com>
2021-06-30 23:33:42 +0200

    Added hydra

---
+++ lisp/init-hydra.el
@@ -0,0 +1,9 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Hydra configs (used mostly for creating menus)
+;;; Code:
+
+(use-package hydra :ensure t )
+
+(provide 'init-hydra)
+;;; init-hydra.el ends here
