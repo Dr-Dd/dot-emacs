81bc1827c725f3d94d8818ea3e7ea6ff0b263d54
Leonardo Idone <idone.leonardo@gmail.com>
2021-07-12 18:09:22 +0200

    Added more eshell integrations

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -3,6 +3,8 @@
 ;;; eshell functions
 ;;; Code:
 
+(evil-set-initial-state 'eshell-mode 'emacs)
+
 (defun my/end-of-line-point ()
   "Return the end of line point of the current line."
   (save-excursion
@@ -12,18 +14,23 @@
       eol-point)))
 
 (defun my/eshell-buffer-working-dir ()
-  "Switch the directory of the buffer named *eshell* to the current working dir."
+  "Switch the directory of the buffer named *eshell* to the current working dir
+and open eshell in new window."
   (interactive)
   (let ((def-dir default-directory))
     (if (not (eq (get-buffer "*eshell*") nil))
-        (with-current-buffer "*eshell*"
-          (eshell-return-to-prompt)
-          (delete-region (point) (my/end-of-line-point))
-          (insert (concat "cd " def-dir))
-          (eshell-send-input))
+        (progn (switch-to-buffer-other-window "*eshell*")
+               (eshell-return-to-prompt)
+               (delete-region (point) (my/end-of-line-point))
+               (insert (concat "cd " def-dir))
+               (eshell-send-input)
+               (eshell/clear))
+      (split-window-below)
+      (other-window 1)
       (eshell))))
 
 (define-key global-map (kbd "C-c x") 'eshell-command)
+(define-key global-map (kbd "C-c C-x") 'my/eshell-buffer-working-dir)
 
 (provide 'init-eshell)
 ;;; init-eshell.el ends here
