ad3c77efb3d7bde0013cfa76d15e22cbc32fa761
drd <idone.leonardo@gmail.com>
2021-08-03 23:07:09 +0200

    Switched to secrets.el

--- .emacs.d/lisp/init-erc.el
+++ .emacs.d/lisp/init-erc.el
@@ -3,6 +3,8 @@
 ;;; Initialization for my erc config via local ZNC bouncer
 ;;; Code:
 
+(require 'secrets)
+
 (use-package erc
   :defer t
   :init
@@ -11,13 +13,16 @@
   :config
   (global-set-key (kbd "C-c i") 'erc-switch-to-buffer))
 
+(use-package erc-hl-nicks :ensure t
+  :defer t
+  :after (erc))
+
 (defvar my/znc-user "drd-admin"
   "User that can access to the ZNC server")
 
-(defvar my/znc-networks-list '("libera" "oftc")
+(defvar my/znc-networks-list '("oftc" "libera")
   "The list of network to which `my/connect-to-znc-networks' connects to.
 It MUST BE SYNCHRONIZED TO the ZNC USER NETWORK LIST (same aliases).")
-
 
 (defun my/connect-to-znc-networks (ZNC-HOST ZNC-PORT)
   "Connect to the ZNC bouncer, which then connects to irc networks specified.
@@ -41,22 +46,12 @@ properly setup a SASL secret for each of your networks from the webconfig"
   (dolist (servname my/znc-networks-list)
     (erc :server ZNC-HOST :port ZNC-PORT :nick my/znc-user
          :password (concat my/znc-user "/" servname ":"
-                           (my/get-authinfo-passwd "localhost" my/znc-user ZNC-PORT)))))
+                           (secrets-get-secret "gen-1" "Local ZNC Admin")))))
 
 (defun my/erc-start-default ()
   "Start default connection with erc."
   (interactive)
   (my/connect-to-znc-networks "127.0.0.1" 1717))
-(use-package erc-hl-nicks :ensure t
-  :defer t
-  :after (erc))
-
-(defun my/get-authinfo-passwd (HOST USER PORT)
-  "Safely get an `auth-sources' USER password.
-The search matches a line in the following format:
-
-machine HOST login USER password *** port PORT"
-  (funcall (plist-get (nth 0 (auth-source-search :host HOST :user USER :port PORT)) :secret)))
 
 (provide 'init-erc)
 ;;; init-erc.el ends here
