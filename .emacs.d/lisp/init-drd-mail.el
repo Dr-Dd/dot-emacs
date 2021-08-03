450da9c7c56468b840540c926a0841b0f875d998
drd <idone.leonardo@gmail.com>
2021-08-03 17:43:53 +0200

    Added spam filtering keybindings, updated gitignore

--- .emacs.d/lisp/init-drd-mail.el
+++ .emacs.d/lisp/init-drd-mail.el
@@ -6,82 +6,64 @@
 
 ;; https://sourceforge.net/projects/isync/files/latest/download
 
+;; Currently it's only linux compatible, shucks!
+(defvar my/notmuch-tag-file "~/.config/notmuch/tag-file")
+
 (use-package notmuch :ensure t
   :defer t
   :config
   ;; Image display
   (when (fboundp 'imagemagick-register-types)
     (imagemagick-register-types))
-  (define-key notmuch-search-mode-map (kbd "C-+") 'my/notmuch-search-add-address-to-smartfilter))
 
-;; (defvar my/notmuch-smartfilter-folder
-;;   (concat my/user-home
-;;           ".git-repos"
-;;           my/path-separator
-;;           "notmuch-smartfilter"
-;;           my/path-separator))
+  ;; Add to spam filter functions
+  (defun my/notmuch-clean-from-addr (address)
+    "Strip a notmuch from address of all useless information except from the
+cleaned-address itself. Return the cleaned-address."
+    (let* ((clean-address
+            (with-temp-buffer
+              (insert address)
+              (goto-char (point-min))
+              (re-search-forward "^\\(.*<\\)")
+              (replace-match "")
+              (goto-char (point-min))
+              (re-search-forward "\\(>.*\\)$")
+              (replace-match "")
+              (buffer-string))))
+      clean-address))
 
-;; (defun my/notmuch-smartfilter--append-address-to-tag-file (address file)
-;;   (let* ((clean-address
-;;           (with-temp-buffer
-;;             (insert address)
-;;             (goto-char (point-min))
-;;             (re-search-forward "^\\(.*<\\)")
-;;             (replace-match "")
-;;             (goto-char (point-min))
-;;             (re-search-forward "\\(>.*\\)$")
-;;             (replace-match "")
-;;             (buffer-string))))
-;;     (with-temp-buffer
-;;       (insert-file-contents (concat my/notmuch-smartfilter-folder file))
-;;       (goto-char (point-min))
-;;       (re-search-forward "\\(addresses=\\)")
-;;       (right-char 1)
-;;       (insert (concat "\n" clean-address " "))
-;;       (write-file (concat my/notmuch-smartfilter-folder file)))
-;;     ))
+  (defun my/notmuch-search-get-from-addr ()
+    "Get full from address attribute from notmuch message interactively on the
+search page."
+    (interactive)
+    (let* ((thread-id (notmuch-search-find-thread-id))
+           (address (with-temp-buffer
+                      (if thread-id
+                          (progn (notmuch-show thread-id)
+                                 (notmuch-show-get-from))
+                        (message "End of search results.")))))
+      (kill-buffer (concat "*notmuch-" thread-id "*"))
+      address))
 
-;; (defun my/notmuch-smartfilter-create-new-tag-file (filename tags)
-;;   (interactive "sFilename: \nsTags: ")
-;;   (with-temp-buffer
-;;     (insert (concat "tags=\"" tags "\"\n"))
-;;     (insert (concat "addresses=\"\""))
-;;     (append-to-file
-;;      (point-min) (point-max)
-;;      (concat my/notmuch-smartfilter-folder filename))))
+  (defun my/notmuch-append-line-from-addr-to-tag-file (address tags tag-file)
+    "Add a filtering line of the following form to TAG-FILE:
 
-;; (defun my/notmuch-search-add-address-to-smartfilter (file)
-;;   (interactive
-;;    (list (completing-read
-;;           "Add to smartfilter tag file: "
-;;           (seq-filter (lambda (v) (numberp (string-match-p "\\(\.sft\\)$" v)))
-;;                       (directory-files my/notmuch-smartfilter-folder))
-;;           nil t "")))
-;;   (let* ((thread-id (notmuch-search-find-thread-id))
-;;          (address
-;;           (with-temp-buffer
-;;             (if thread-id
-;;                 (progn (notmuch-show thread-id)
-;;                        (notmuch-show-get-from))
-;;               (message "End of search results.")))))
-;;     (kill-buffer (concat "*notmuch-" thread-id "*"))
-;;     (my/notmuch-smartfilter--append-address-to-tag-file
-;;      address file)))
++tag1 -tag2 ... -- from:address@domain.org
 
-;; (defun my/notmuch-run-smartfilter ()
-;;   (interactive)
-;;   (shell-command (concat my/git-repos-directory
-;;                          "notmuch-smartfilter"
-;;                          my/path-separator
-;;                          "notmuch-smartfilter.sh")))
+using supplied TAGS, ADDRESS."
+    (with-temp-buffer
+      (insert (concat tags " -- from:" address "\n"))
+      (append-to-file (point-min) (point-max) tag-file)))
 
-;; (defun my/notmuch-run-smartfilter-full ()
-;;   (interactive)
-;;   (shell-command (concat my/git-repos-directory
-;;                          "notmuch-smartfilter"
-;;                          my/path-separator
-;;                          "notmuch-smartfilter.sh -f")))
+  (defun my/notmuch-search-add-from-addr-to-spam-filter ()
+    "Interactively add a from address to `my/notmuch-tag-file' filtered from
+the tags \"+spam -inbox -unread\""
+    (interactive)
+    (let* ((addr (my/notmuch-clean-from-addr (my/notmuch-search-get-from-addr))))
+      (my/notmuch-append-line-from-addr-to-tag-file addr "+spam -inbox -unread" my/notmuch-tag-file)))
 
+  ;; Keybindings
+  (define-key notmuch-search-mode-map (kbd "C-+") 'my/notmuch-search-add-from-addr-to-spam-filter))
 
 (provide 'init-drd-mail)
 ;;; init-drd-mail.el ends here
