7c0560c982b7aea8bcfba3127e4700346f1547b7
drd <idone.leonardo@gmail.com>
2021-09-02 16:48:30 +0200

    Added char test for current typeface

--- .emacs.d/lisp/init-fcitx.el
+++ .emacs.d/lisp/init-fcitx.el
@@ -3,8 +3,12 @@
 ;;; Just my init file
 ;;; Code:
 
+;; Your typeface should be able to print the following characters
+;; かな
+;; if not, add the required typeface(s) to your distribution
+
 ;; DBus is stuck with fcitx4, while fcitx5 service name is different
-;; , so FIXME
+;; so FIXME in fcitx.el
 (use-package fcitx
   :ensure t
   :config
