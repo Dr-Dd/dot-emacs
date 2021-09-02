7309f14e617265c879377332663398d8db09a76d
drd <idone.leonardo@gmail.com>
2021-09-02 17:53:23 +0200

    Simple workaround for evil fcitx

--- .emacs.d/lisp/init-evil-mode.el
+++ .emacs.d/lisp/init-evil-mode.el
@@ -21,5 +21,13 @@
   :config
   (diminish 'evil-commentary-mode))
 
+;; Simple workaround for fcitx in evil-insert mode
+;; Potentially better than fcitx.el
+;; https://yoshitia.hatenablog.com/entry/2015/04/02/213710
+(add-hook 'evil-normal-state-entry-hook
+	  (lambda ()
+	    (if (string= "2\n" (shell-command-to-string "fcitx-remote"))
+		(shell-command "fcitx-remote -c"))))
+
 (provide 'init-evil-mode)
 ;;; init-evil-mode.el ends here
